8eab62885a34f1e3c5ceaec67463152852472341
Adam Paulukanis <adam.paulukanis@gmail.com>
2021-12-02 18:41:05 +0100

    Bounce the ball off the walls

--- main.js
+++ main.js
@@ -8,10 +8,11 @@ let x = canvas.width / 2;
 let y = canvas.height / 2;
 let dx = 2;
 let dy = -2;
+const ballRadius = 10;
 
 function drawBall() {
   ctx.beginPath();
-  ctx.arc(x, y, 10, 0, Math.PI * 2);
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
   ctx.fillStyle = '#0095dd';
   ctx.fill();
   ctx.closePath();
@@ -22,6 +23,13 @@ function draw() {
   drawBall();
   x += dx;
   y += dy;
+
+  if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+    dy = -dy;
+  }
+  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+    dx = -dx;
+  }
 }
 
 setInterval(draw, 10);
