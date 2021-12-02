7c45a71508a3c7e439e4fc0be81599b444ec56a9
Adam Paulukanis <adam.paulukanis@gmail.com>
2021-12-02 19:08:26 +0100

    Paddle and keyboard controls

--- main.js
+++ main.js
@@ -10,6 +10,13 @@ let dx = 2;
 let dy = -2;
 const ballRadius = 10;
 
+const paddleHeight = 10;
+const paddleWidth = 75;
+let paddleX = (canvas.width - paddleWidth) / 2;
+
+let rightPressed = false;
+let leftPressed = false;
+
 function drawBall() {
   ctx.beginPath();
   ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
@@ -18,9 +25,18 @@ function drawBall() {
   ctx.closePath();
 }
 
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = '#0095dd';
+  ctx.fill();
+  ctx.closePath();
+}
+
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
+  drawPaddle();
   x += dx;
   y += dy;
 
@@ -30,6 +46,37 @@ function draw() {
   if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
     dx = -dx;
   }
+
+  if (rightPressed) {
+    paddleX += 7;
+    if (paddleX + paddleWidth > canvas.width) {
+      paddleX = canvas.width - paddleWidth;
+    }
+  } else if (leftPressed) {
+    paddleX -= 7;
+    if (paddleX < 0) {
+      paddleX = 0;
+    }
+  }
 }
+
+function keyDownHandler(e) {
+  if (e.key == 'Right' || e.key == 'ArrowRight') {
+    rightPressed = true;
+  } else if (e.key == 'Left' || e.key == 'ArrowLeft') {
+    leftPressed = true;
+  }
+}
+
+function keyUpHandler(e) {
+  if (e.key == 'Right' || e.key == 'ArrowRight') {
+    rightPressed = false;
+  } else if (e.key == 'Left' || e.key == 'ArrowLeft') {
+    leftPressed = false;
+  }
+}
+
+document.addEventListener('keydown', keyDownHandler, false);
+document.addEventListener('keyup', keyUpHandler, false);
 
 setInterval(draw, 10);
